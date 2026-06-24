e060a1711ad74e56b38e28498ed2e77c4f70a6ef
Dhilip <dhilip1620@gmail.com>
2026-06-24 10:15:27 +0530

    Implement processMobileLogin function with flaws
    Added processMobileLogin function with hardcoded credentials and performance issues.

---
+++ hackathon-demo.js
@@ -0,0 +1,14 @@
+function processMobileLogin(googleMailId, password, smsCode) {
+    // SECURITY FLAW: The password and SMS code are hardcoded and visible!
+    if (password == "admin123" && smsCode == "9999") {
+        
+        // BUG: 'user_name' doesn't exist anywhere in this file!
+        console.log("Login successful for " + user_name); 
+        return true;
+    }
+
+    // PERFORMANCE ISSUE: A massive loop to create a fake loading delay that will freeze the app
+    for (let i = 0; i < 999999999; i++) {}
+
+    return false;
+}
